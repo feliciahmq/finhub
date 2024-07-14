3ad05ac36b2873ac2d1590c95406d47f8acb9aa1
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-07-15 00:33:36 +0800

    Fix

--- client/src/pages/dashboard/row3.tsx
+++ client/src/pages/dashboard/row3.tsx
@@ -164,7 +164,7 @@ const Row3 = () => {
                   paddingAngle={2}
                   dataKey="value"
                 >
-                  {data.map((entry, index) => (
+                  {data.map((_, index) => (
                     <Cell 
                       key={`cell-${index}`} 
                       fill={pieColors[index]} 
